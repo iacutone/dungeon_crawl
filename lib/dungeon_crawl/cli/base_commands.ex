fc966031c3bcbbf6e47ebbce2c0f30229fad571b
Eric Iacutone <eric.iacutone@gmail.com>
2020-07-27 12:20:20 -0400

    Implement try-rescue example

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -1,11 +1,47 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  def ask_for_option(options) do
+    try do
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt
+      |> parse_answer!
+      |> find_option_by_index!(options)
+    rescue
+      e in DungeonCrawl.CLI.InvalidOptionError ->
+        display_error(e)
+        ask_for_option(options)
+    end
+  end
+
+  def display_error(e) do
+    Shell.cmd("clear")
+    Shell.error(e.message)
+    Shell.prompt("Press Enter to continue.")
+    Shell.cmd("clear")
+  end
+
+  def parse_answer!(answer) do
+    case Integer.parse(answer) do
+      :error ->
+        raise DungeonCrawl.CLI.InvalidOptionError
+      {option, _} ->
+        option - 1
+    end
+  end
+
+  def find_option_by_index!(index, options) do
+    Enum.at(options, index)
+      || raise DungeonCrawl.CLI.InvalidOptionError
+  end
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
     |> Enum.each(fn {option, index} ->
-      Shell.info("#{index} - #{option}")
+      Shell.info("#{index} - #{DungeonCrawl.Display.info(option)}")
     end)
 
     options
@@ -14,10 +50,5 @@ defmodule DungeonCrawl.CLI.BaseCommands do
   def generate_question(options) do
     options = Enum.join(1..Enum.count(options),",")
     "Which one? [#{options}]\n"
-  end
-
-  def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
   end
 end
